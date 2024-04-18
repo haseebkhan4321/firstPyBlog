dfa60d21e7b8b130daa8c8185f93adb1ab5ab42d
Haseeb khan <haseeb.khan@ozteckwork.com>
2024-04-18 17:03:10 +0500

    models added

--- post/models.py
+++ post/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+from category.models import Category
+from comment.models import Comment
+from tag.models import Tag
+
 
 # Create your models here.
 
@@ -8,6 +12,12 @@ class Post(models.Model):
     content = models.TextField(blank=True, null=True)
     author_name = models.CharField(max_length=255, blank=True, null=True)
     image = models.ImageField(default="default.jpg")
+    category = models.OneToOneField(
+        Category,
+        on_delete=models.CASCADE,
+    )
+    tag = models.ManyToManyField(Tag)
+    comment = models.ManyToManyField(Comment)
     published_at = models.DateTimeField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
